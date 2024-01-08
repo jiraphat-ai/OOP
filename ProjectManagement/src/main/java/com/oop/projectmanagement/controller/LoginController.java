ec924680eee647638f241dc9ff958742efb4e0cf
ballalone1234 <ballpol.127@gmail.com>
2024-01-08 23:05:35 +0700

    888989

--- ProjectManagement/src/main/java/com/oop/projectmanagement/controller/LoginController.java
+++ ProjectManagement/src/main/java/com/oop/projectmanagement/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.oop.projectmanagement.controller;
-
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.oop.projectmanagement.FirebaseInitializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
