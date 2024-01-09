e41d16e280a30c48ebecb5835674b2297f3e5acc
Taywin Tomthisang <B6501730@g.sut.ac.th>
2024-01-09 18:30:55 +0700

    Refactor FirebaseInitializer class to use ClassPathResource for service account key

--- ProjectManagement/src/main/java/com/oop/projectmanagement/FirebaseInitializer.java
+++ ProjectManagement/src/main/java/com/oop/projectmanagement/FirebaseInitializer.java
@@ -6,37 +6,31 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.cloud.firestore.Firestore;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
-
-
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-
 @Service
-    public class FirebaseInitializer {
-        public void initialize() {
-            try {
-               //file path to the service account key json file
-               File file = new File(ClassLoader.getSystemResource("serviceAccountKey.json").getFile());
+public class FirebaseInitializer {
 
-                FirebaseOptions options = FirebaseOptions.builder()
-                        .setCredentials(GoogleCredentials.fromStream(new FileInputStream(file)))
-                        .build();
+    public void initialize() {
+        try (InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
-                if (FirebaseApp.getApps().isEmpty()) {
-                    FirebaseApp.initializeApp(options);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-     public Firestore getDb() {
-            initialize();
-        return FirestoreClient.getFirestore();}
-}
+    }
 
-   
+    public Firestore getDb() {
+        initialize();
+        return FirestoreClient.getFirestore();
+    }
+}
