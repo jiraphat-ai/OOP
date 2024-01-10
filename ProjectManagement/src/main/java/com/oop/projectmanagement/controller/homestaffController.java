fa266240eb35b1fd6080615b003a590292d53e9c
Taywin Tomthisang <B6501730@g.sut.ac.th>
2024-01-10 14:09:52 +0700

    Refactor homestaffController and homestaff.html

--- ProjectManagement/src/main/java/com/oop/projectmanagement/controller/homestaffController.java
+++ ProjectManagement/src/main/java/com/oop/projectmanagement/controller/homestaffController.java
@@ -4,14 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.oop.projectmanagement.FirebaseInitializer;
 
 @Controller
-public class homestaffController {
+public class HomeStaffController {
 
     @Autowired
     private FirebaseInitializer firebaseInitializer;
@@ -20,5 +25,31 @@ public class homestaffController {
     public String portfileUser(Model model) {
 
         return "homestaff";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(
+    @RequestParam("studentId") String studentId,
+    @RequestParam("password") String password,
+    @RequestParam("firstName") String firstName,
+    @RequestParam("lastName") String lastName) {
+
+    Firestore db = firebaseInitializer.getDb(); // Use getDb() instead of getFirebase()
+
+    Map<String, Object> user = new HashMap<>();
+    user.put("studentId", studentId);
+    user.put("password", password);
+    user.put("firstName", firstName);
+    user.put("lastName", lastName);
+
+    try {
+        ApiFuture<DocumentReference> addedDocRef = db.collection("users").add(user);
+    } catch (Exception e) {
+        e.printStackTrace();
+        return "error";
+    }
+
+    return "redirect:/homestaff";
+
     }
 }
