c5a4d8bee5b94f40144451b8227273f0b3362e29
ballalone1234 <ballpol.127@gmail.com>
2024-01-13 19:34:35 +0700

    Set up ajax Create template_student.html and Create menu_bar_student.html

--- ProjectManagement/src/main/java/com/oop/projectmanagement/controller/MenubarController.java
+++ ProjectManagement/src/main/java/com/oop/projectmanagement/controller/MenubarController.java
@@ -1,40 +1,44 @@
 package com.oop.projectmanagement.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import javax.servlet.http.HttpSession;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.oop.projectmanagement.FirebaseInitializer;
 
+@Controller
 public class MenubarController {
-    FirebaseInitializer firebaseInitializer;
-public void getUserData(HttpSession session, Model model) {
-    String username = (String) session.getAttribute("username");
-    Firestore db = firebaseInitializer.getDb();
-    try {
-        ApiFuture<QuerySnapshot> query = db.collection("useraccount").whereEqualTo("username", username).get();
-        QuerySnapshot querySnapshot = query.get();
-        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
-        if (!documents.isEmpty()) {
-            DocumentSnapshot document = documents.get(0);
-            // Fetch the values you need from the document
-            String firstName = document.getString("firstName");
-            String lastName = document.getString("lastName");
-            // Add the values to the model
-            model.addAttribute("firstName", firstName);
-            model.addAttribute("lastName", lastName);
-        }
-    } catch (Exception e) {
-        model.addAttribute("error", "An error occurred: " + e.getMessage());
+
+    @Autowired
+    private FirebaseInitializer firebaseInitializer;
+
+
+    @GetMapping("/menu_bar_student")
+
+    public String getBar(HttpSession session) {
+        String username = (String) session.getAttribute("username");
+        String firstName = (String) session.getAttribute("firstName");
+        String lastName = (String) session.getAttribute("lastName");
+        // Now you can use the username, firstName, and lastName
+        return "menu_bar_student";
+
     }
-}
 
 }
