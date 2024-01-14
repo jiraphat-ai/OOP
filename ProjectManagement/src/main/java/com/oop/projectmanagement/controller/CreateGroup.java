96a1334a1c71155b0f28146a4420aeb1d35d9e3e
ballalone1234 <ballpol.127@gmail.com>
2024-01-15 02:07:32 +0700

    fetch option tag in creategroup.html

--- ProjectManagement/src/main/java/com/oop/projectmanagement/controller/CreateGroup.java
+++ ProjectManagement/src/main/java/com/oop/projectmanagement/controller/CreateGroup.java
@@ -1,9 +1,13 @@
 package com.oop.projectmanagement.controller;
 
+import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.oop.projectmanagement.model.Group;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -12,6 +16,7 @@ import com.oop.projectmanagement.FirebaseInitializer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @Controller
 public class CreateGroup {
@@ -21,13 +26,30 @@ public class CreateGroup {
 
     @GetMapping("/creategroup")
 
-    public String getUserinfo(HttpSession session) {
+    public String getUserinfo(HttpSession session , Model model) {
         String username = (String) session.getAttribute("username");
         String firstName = (String) session.getAttribute("firstName");
         String lastName = (String) session.getAttribute("lastName");
+        model.addAttribute("tags", getTags());
         // Now you can use the username, firstName, and lastName
         return "creategroup";
 
+    }
+
+    //get Tags from database firestore
+    private List<Map<String, Object>> getTags() {
+        Firestore db = firebaseInitializer.getDb();
+        List<Map<String, Object>> tags = new ArrayList<>();
+        try {
+            ApiFuture<QuerySnapshot> querySnapshot = db.collection("tags").get();
+            List<QueryDocumentSnapshot> documents = querySnapshot.get().getDocuments();
+            for (QueryDocumentSnapshot document : documents) {
+                tags.add(document.getData());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return tags;
     }
     @PostMapping("/createGroup")
     @ResponseBody
