5f456bbbf8f1d742e54b95bd0b92e643fdf2830c
Rithnapa <lolrithnapa@gmail.com>
2024-01-22 14:10:47 +0700

    SEARCH WITH TAG

--- ProjectManagement/src/main/java/com/oop/projectmanagement/controller/JoinGroup.java
+++ ProjectManagement/src/main/java/com/oop/projectmanagement/controller/JoinGroup.java
@@ -3,19 +3,19 @@ package com.oop.projectmanagement.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import javax.servlet.http.HttpSession;
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot; 
 import com.google.cloud.firestore.QuerySnapshot;
 import com.oop.projectmanagement.FirebaseInitializer;
-import com.oop.projectmanagement.model.GroupFordetail;
 import com.google.cloud.firestore.Query;
 
 
@@ -41,40 +41,68 @@ public class JoinGroup extends CustomControl {
     //SHOW GROUP DETAIL SECTION
     @GetMapping("/moregroupdetail")
     public String getGroupMoreDetail(@RequestParam String documentId, Model model) {
-        System.out.println("documentId " + documentId);
+        //System.out.println("documentId " + documentId);
         List<Map<String, Object>> groupdetail = searchDocumentById(documentId);
         model.addAttribute("groupdetail", groupdetail);
         return "/groupDetailFragment";
     }
- 
+
 
     @GetMapping("/searchgroup")
-    public String searchGroup(@RequestParam("subjectID") String subjectID, @RequestParam("section") int section, Model model) {
-        List<Map<String, Object>> groups = getGroupsBySubjectId(subjectID, section);
+    public String searchGroup(@RequestParam("subjectID") String subjectID, @RequestParam("section") int section, @RequestParam(value = "tag", required = false) String[] tag, Model model) {
+        List<String> tagList = tag != null ? Arrays.asList(tag) : null; // Convert the array to a list
+        List<Map<String, Object>> groups = getGroupsBySubjectId(subjectID, section, tagList);
+        //System.out.println("tag: " + tagList);
         for (Map<String, Object> group : groups) {
-            System.out.println("joinedMember: " + group.get("joinedMember"));
+            //System.out.println("joinedMember: " + group.get("joinedMember"));
         }
         model.addAttribute("groups", groups);
         return "/joingroup"; // return the name of the view that will display the groups
     }
-    public List<Map<String, Object>> getGroupsBySubjectId(String subjectID, int section) {
+    public List<Map<String, Object>> getGroupsBySubjectId(String subjectID, int section, List<String> tag) {
         Firestore db = firebaseInitializer.getDb();
         List<Map<String, Object>> groups = new ArrayList<>();
-        // i want to get the group that have the same any text in subjectID and section
+        
+        
         try {
             Query query = db.collection("group").whereEqualTo("subjectID", subjectID);
+            
             if (section != 0) {
                 query = query.whereEqualTo("section", section);
             }
+            
+            if (tag != null && !tag.isEmpty()) {
+                List<QuerySnapshot> snapshots = new ArrayList<>();
+                    for (String t : tag) {
+                    Query tagQuery = query.whereArrayContains("tag", t);
+                    //System.out.println("tagQuery: " + tagQuery);
+                    snapshots.add(tagQuery.get().get());
+                    }
 
-            ApiFuture<QuerySnapshot> querySnapshot = query.get();
-            List<QueryDocumentSnapshot> documents = querySnapshot.get().getDocuments();
-            for (QueryDocumentSnapshot document : documents) {
-                Map<String, Object> groupData = document.getData();
-                groupData.put("documentId", document.getId()); // Add document ID to the group data
-                groups.add(groupData);
-                lastsearchgroup = groups;
-            }
+                // Combine the results of the tag queries
+                for (QuerySnapshot snapshot : snapshots) {
+                    for (DocumentSnapshot document : snapshot.getDocuments()) {
+                        Map<String, Object> groupData = document.getData();
+                        groupData.put("documentId", document.getId());
+                        groups.add(groupData);
+                        //System.out.println("groupData: " + groupData);
+                        lastsearchgroup = groups;
+                        }
+                    }      
+                } else {
+                ApiFuture<QuerySnapshot> querySnapshot = query.get();
+                List<QueryDocumentSnapshot> documents = querySnapshot.get().getDocuments();
+            
+                for (QueryDocumentSnapshot document : documents) {
+                    Map<String, Object> groupData = document.getData();
+                    groupData.put("documentId", document.getId());
+                    groups.add(groupData);
+                    //System.out.println("groupData: " + groupData);
+                    lastsearchgroup = groups;
+                    }           
+                } 
+
+            
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
