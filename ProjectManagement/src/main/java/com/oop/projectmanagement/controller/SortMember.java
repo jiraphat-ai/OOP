9a1ba8619f38cd2fdfd4a931bd64d3c7d45e1f1c
Taywin Tomthisang <B6501730@g.sut.ac.th>
2024-01-26 00:14:09 +0700

    SortMember class and update sorting functionality in homestaff.html

--- ProjectManagement/src/main/java/com/oop/projectmanagement/controller/SortMember.java
+++ ProjectManagement/src/main/java/com/oop/projectmanagement/controller/SortMember.java
@@ -1,25 +1,18 @@
 package com.oop.projectmanagement.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.oop.projectmanagement.model.Member;
 
-import java.lang.reflect.Member;
-import java.util.Comparator;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 
-public abstract class SortMember {
-    protected final MemberService memberService;
+abstract public class SortMember {
 
-    public SortMember (MemberService memberService) {
-        this.memberService = memberService;
-    }
+    abstract public List<Member> sortMember();
 
-    protected abstract Comparator<Member> getComparator();
+    public List<Member> memberSearchResult;
 
-    @GetMapping
-    public List<Member> sort() {
-        List<Member> Account = memberService.findAll();
-        Account.sort(getComparator());
-        return Account;
+    public List<Member> setMember(HttpSession session) {
+        memberSearchResult = (List<Member>) session.getAttribute("lastsearchmember");
+        return memberSearchResult;
     }
 }
---
+++ ProjectManagement/src/main/java/com/oop/projectmanagement/controller/sortMemberZtoA.java
@@ -0,0 +1,24 @@
+package com.oop.projectmanagement.controller;
+
+import com.oop.projectmanagement.model.Member;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortMemberZtoA extends SortMember {
+
+    public List<Member> sortMember() {
+        List<Member> sortedMembers = new ArrayList<>(memberSearchResult);
+        Collections.sort(sortedMembers, new Comparator<Member>() {
+            @Override
+            public int compare(Member member1, Member member2) {
+                String memberName1 = member1.getName();
+                String memberName2 = member2.getName();
+                return memberName2.compareToIgnoreCase(memberName1);
+            }
+        });
+        return sortedMembers;
+    }
+}
